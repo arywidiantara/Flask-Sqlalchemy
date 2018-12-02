a3f689f1704d395228c4d8ab2bc6ae481f5b0202
arywidiantara <arywidiantara33@gmail.com>
2018-12-02 12:34:03 +0700

    [add] config for env

--- config/env.py
+++ config/env.py
@@ -8,20 +8,26 @@ class Environment():
             :return string:
         '''
         param = {
-            'APP_LOC': os.getenv('APP_LOC'),
+            'APP_ENV': os.environ.get('APP_ENV'),
+            'APP_KEY': os.environ.get('APP_KEY'),
+            'APP_TIMEZONE': os.environ.get('APP_TIMEZONE'),
+            'APP_HOST': os.environ.get('APP_HOST'),
+            'APP_PORT': os.environ.get('APP_PORT'),
+            'APP_DEBUG': os.environ.get('APP_DEBUG'),
+            'APP_LOC': os.environ.get('APP_LOC'),
+            'DB_CONNECTION': os.getenv('DB_CONNECTION'),
+            'DB_HOST': os.getenv('DB_HOST'),
+            'DB_PORT': os.getenv('DB_PORT'),
+            'DB_DATABASE': os.getenv('DB_DATABASE'),
+            'DB_USERNAME': os.getenv('DB_USERNAME'),
+            'DB_PASSWORD': os.getenv('DB_PASSWORD'),
+            'SLACK_WEBHOOK': os.getenv('SLACK_WEBHOOK'),
+            'SLACK_CHANNEL': os.getenv('SLACK_CHANNEL'),
             'FILESYSTEM_DRIVER': os.getenv('FILESYSTEM_DRIVER'),
             'AWS_ACCESS_KEY': os.getenv('AWS_ACCESS_KEY'),
             'AWS_ACCESS_SECRET': os.getenv('AWS_ACCESS_SECRET'),
             'AWS_REGION': os.getenv('AWS_REGION'),
             'AWS_BUCKET': os.getenv('AWS_BUCKET'),
-            'BRIGHTCOVE_AUTH_ENDPOINT': os.getenv('BRIGHTCOVE_AUTH_ENDPOINT'),
-            'BRIGHTCOVE_INGEST_ENDPOINT': os.getenv('BRIGHTCOVE_INGEST_ENDPOINT'),
-            'BRIGHTCOVE_ENDPOINT': os.getenv('BRIGHTCOVE_ENDPOINT'),
-            'BRIGHTCOVE_REGION': os.getenv('BRIGHTCOVE_REGION'),
-            'BRIGHTCOVE_API_KEY': os.getenv('BRIGHTCOVE_API_KEY'),
-            'BRIGHTCOVE_ACCOUNT_ID': os.getenv('BRIGHTCOVE_ACCOUNT_ID'),
-            'BRIGHTCOVE_CLIENT_ID': os.getenv('BRIGHTCOVE_CLIENT_ID'),
-            'BRIGHTCOVE_CLIENT_SECRET': os.getenv('BRIGHTCOVE_CLIENT_SECRET'),
         }
 
         # check if not empty
